fdb13d7a41482b9e7311f0ba7bd022918074ef49
Lucas Alves <120467433+LucasAAraujo@users.noreply.github.com>
2023-10-27 11:27:37 -0300

    Add files via upload

---
+++ picpay/src/main/java/com/picpay/picpay/dtos/NotificationDTO.java
@@ -0,0 +1,5 @@
+package com.picpay.picpay.dtos;
+
+public record NotificationDTO(String email, String message) {
+
+}
---
+++ picpay/src/main/java/com/picpay/picpay/services/NotificationService.java
@@ -0,0 +1,29 @@
+package com.picpay.picpay.services;
+
+
+import com.picpay.picpay.domain.user.User;
+import com.picpay.picpay.dtos.NotificationDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class NotificationService {
+    @Autowired
+    private RestTemplate restTamplate;
+
+    public void sendNotification(User user, String message) throws Exception{
+        String email = user.getEmail();
+        NotificationDTO notificationRequest = new NotificationDTO(email, message);
+
+        ResponseEntity<String> notificationResponse = restTamplate.postForEntity("http://o4d9z.mocklab.io/notify", notificationRequest, String.class);
+
+        if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
+            System.out.println("Erro ao enviar notificação");
+            throw new Exception("Serviço de notificação está fora do ar");
+        }
+    }
+}
